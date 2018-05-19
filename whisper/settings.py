c7a75b0b3653291a09414c6dc28ab7ad20b2a57a
Alireza Savand <alireza.savand@gmail.com>
2018-05-19 20:16:41 +0300

    Add add heroku domain to allowed hosts

--- whisper/settings.py
+++ whisper/settings.py
@@ -60,7 +60,7 @@ MESSAGE_TYPES_LIST = [
 # SECURITY WARNING: keep the secret key used in production secret! And don't use debug=True in production!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'imasecret')
 DEBUG = True
-ALLOWED_HOSTS = ["192.168.56.1", ]
+ALLOWED_HOSTS = ["192.168.56.1", 'savandbros-whisper.herokuapp.com', ]
 
 # Application definition
 INSTALLED_APPS = [
