14d5cd5b29a656a55c494e7118ba9f0c0a0701f7
Alireza Savand <alireza.savand@gmail.com>
2018-05-20 22:58:45 +0300

    Add small amount of code hinting

--- chat/exceptions.py
+++ chat/exceptions.py
@@ -3,6 +3,6 @@ class ClientError(Exception):
     Custom exception class that is caught by the websocket receive()
     handler and translated into a send back to the client.
     """
-    def __init__(self, code):
+    def __init__(self, code: str):
         super().__init__(code)
-        self.code = code
+        self.code: str = code
