6bc1977626711a66fb43154bd2f8e118622e7501
Alireza Savand <alireza.savand@gmail.com>
2018-06-15 19:42:07 +0300

    Having all of them in app.js

--- static/assets/app.js
+++ static/assets/app.js
@@ -2,6 +2,18 @@
  * App module
  */
 var app = angular.module("whisper", []);
+var webrtc = new SimpleWebRTC({
+  // the id/element dom element that will hold "our" video
+  localVideoEl: 'localVideo',
+  // the id/element dom element that will hold remote videos
+  remoteVideosEl: 'remoteVideos',
+  // immediately ask for camera access
+  autoRequestMedia: true
+});
+webrtc.on('readyToCall', function () {
+  // you can name it anything
+  webrtc.joinRoom("amir-alireza");
+});
 
 /**
  * App config
